43d987bde81ecd21513277c2fc2185dcc0799a4f
Phil Newton <phil@sodaware.net>
2017-03-28 16:39:14 -0400

    Add tests to check what is sent to beautification process

--- test/php-beautifier-test.el
+++ test/php-beautifier-test.el
@@ -1,6 +1,24 @@
 ;;; php-beautifier-test.el --- Tests for php-beautifier.el
 
-;; Indentation method checks
+;; Execution tests
+
+;; Test that the entire buffer is sent (will be point 1 => 1)
+(ert-deftest php-beautifier-tests/sends-entire-buffer-when-empty ()
+  (with-mock
+   (mock (shell-command-on-region 1 1 * * t * t) => 0)
+   (php-beautifier-format-buffer)))
+
+;; Test that the entire buffer is sent when there is content. Inserts 10 chars
+;; into the scratch buffer and checks it is sent.
+(ert-deftest php-beautifier-tests/sends-entire-buffer-when-full ()
+  (with-mock
+   (switch-to-buffer "*scratch*")
+   (insert "1234567890")
+   (mock (shell-command-on-region 1 11 * * t * t) => 0)
+   (php-beautifier-format-buffer)))
+
+
+;; Indentation method parameter checks
 
 (ert-deftest php-beautifier-test/adds-space-indent-method-correctly ()
   (let ((php-beautifier-indent-method "spaces"))
--- test/test-helper.el
+++ test/test-helper.el
@@ -1,5 +1,11 @@
-;; Add library path
-(add-to-list 'load-path
-             (file-name-directory (directory-file-name (file-name-directory load-file-name))))
+;; Load mock libraries
+(require 'el-mock)
+(eval-when-compile
+  (require 'cl))
+
+;; Set directory for the module
+(setq module-directory
+      (file-name-directory (directory-file-name (file-name-directory load-file-name))))
+(add-to-list 'load-path module-directory)
 
 (require 'php-beautifier)
